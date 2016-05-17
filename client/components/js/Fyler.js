1f07480c43615b22bb63d1eb0ece67437bfabfce
Saurabh SHarma <saurshaz@gmail.com>
2016-05-17 16:14:50 +0530

    rename, and cleanup

--- client/components/js/Fyler.js
+++ client/components/js/Fyler.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const superagent = require('superagent')
 const FYLER_SERVER_URL = 'http://0.0.0.0:3000'
 // self.config = JSON.parse(self.root.querySelector('data-config').innerHTML)
 
@@ -10,15 +9,13 @@ module.exports = {
       method: 'PUT',
       url: FYLER_SERVER_URL + '/fyler',
       data: config
-    })
-      .done(function (r) {
-        debugger
-        console.log({
-          'message': 'From Fyler - response',
-          'text': r
-        })
-        cb(undefined, r)
-      }).error(function (err) {
+    }).done(function (r) {
+      console.log({
+        'message': 'From Fyler - response',
+        'text': JSON.parse(r)
+      })
+      cb(undefined, r)
+    }).error(function (err) {
       console.error({
         'message': 'From Fyler - err',
         'text': err
